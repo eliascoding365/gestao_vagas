3bee9210f093b3070d648480a3fb547f7cc1c1c4
eliascoding365 <eliasmontan10@gmail.com>
2024-04-24 13:39:24 -0300

    Docker settings and entity update

--- src/main/java/br/com/eliasmontan/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/eliasmontan/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,29 @@
 package br.com.eliasmontan.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
 @Data
+@Entity( name="candidate" )
 public class CandidateEntity {
 
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
   @NotBlank
   @Pattern(regexp = "\\S+", message = "O campo [usernam] não deve conter espaco")
@@ -22,7 +34,11 @@ public class CandidateEntity {
   
   @Length (min= 10, max= 20)
   private String password;
-
   private String description;
   private String curriculum;
+
+  @CreationTimestamp()
+  private LocalDateTime createdAT;
+
+
 }
