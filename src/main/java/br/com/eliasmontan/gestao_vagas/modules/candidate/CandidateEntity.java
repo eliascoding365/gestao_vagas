690fc73222d6daf87b9c298b26c72390638d8e87
eliascoding365 <eliasmontan10@gmail.com>
2024-04-29 13:01:34 -0300

    Corrigi essa bct vtmnc

--- src/main/java/br/com/eliasmontan/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/eliasmontan/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -16,29 +16,25 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
-@Entity( name="candidate" )
+@Entity(name = "candidate")
 public class CandidateEntity {
-
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   private String name;
-  @NotBlank
-  @Pattern(regexp = "\\S+", message = "O campo [usernam] não deve conter espaco")
-  private String username;
 
-  @Email (message = "o campo [email] deve conter email valido")
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+  private String username;
+  @Email(message = "O campo [email] deve conter um e-mail válido")
   private String email;
-  
-  @Length (min= 10, max= 20)
+  @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
   private String password;
   private String description;
   private String curriculum;
 
-  @CreationTimestamp()
-  private LocalDateTime createdAT;
-
-
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
