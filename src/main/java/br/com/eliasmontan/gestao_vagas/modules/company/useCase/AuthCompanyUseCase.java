2b3076ec4292f0ce655ae1914f7f789af8387e7d
eliascoding365 <eliasmontan10@gmail.com>
2024-06-27 22:38:27 -0300

    Finish Company UseCase

--- src/main/java/br/com/eliasmontan/gestao_vagas/modules/company/useCase/AuthCompanyUseCase.java
+++ src/main/java/br/com/eliasmontan/gestao_vagas/modules/company/useCase/AuthCompanyUseCase.java
@@ -3,9 +3,13 @@ package br.com.eliasmontan.gestao_vagas.modules.company.useCase;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.eliasmontan.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.eliasmontan.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -16,13 +20,16 @@ public class AuthCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Value("${security.token.service}")
+    private String secretKey;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
 
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
             () -> {
-                throw new UsernameNotFoundException("company not found");
+                throw new UsernameNotFoundException("Username/password incorrect");
             });
 
            ///Verificar a senha são iguais
@@ -32,5 +39,11 @@ public class AuthCompanyUseCase {
             throw new AuthenticationException();
            }
 
+           Algorithm algorithm = Algorithm.HMAC256(secretKey);
+           var token = JWT.create().withIssuer("javagar")
+           .withSubject(company.getId().toString())
+           .sign(algorithm);
+
+           return token;
     }
 }
