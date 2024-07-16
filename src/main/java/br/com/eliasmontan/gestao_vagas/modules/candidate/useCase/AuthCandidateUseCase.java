94ee096dfdd3bf6bd898479af848e8142cbddffe
eliascoding365 <eliasmontan10@gmail.com>
2024-07-16 14:28:32 -0300

    Create candidate token - sign, expiration, roles, DTO

---
+++ src/main/java/br/com/eliasmontan/gestao_vagas/modules/candidate/useCase/AuthCandidateUseCase.java
@@ -0,0 +1,59 @@
+package br.com.eliasmontan.gestao_vagas.modules.candidate.useCase;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.eliasmontan.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.eliasmontan.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.eliasmontan.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+
+public class AuthCandidateUseCase {
+    
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException{
+        var candidate = this.candidateRepository.findByUsernameOrUsername(authCandidateRequestDTO.username())
+        .orElseThrow(() -> {
+            throw new UsernameNotFoundException("Invalid Username/password");
+        });
+        
+        var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if(!passwordMatches){
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create()
+        .withIssuer("javagas")
+        .withSubject(candidate.getId().toString())
+        .withClaim("roles", Arrays.asList("candidate"))
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+        .sign(algorithm)
+        ;
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+        .access_token(token)
+        .build();
+
+        return authCandidateResponse;
+    }
+}
